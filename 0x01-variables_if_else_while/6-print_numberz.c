290f2f465e5c654fd7b61681d3dd32904d8380c4
Ghuchii <mwangikamau15@gmail.com>
2023-03-19 14:24:37 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - start of programm
+ * 20/03/2023
+ * Return: end of program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
