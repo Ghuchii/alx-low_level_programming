c9dff3ac82af3cf1932f337d825020cf024b5590
Ghuchii <mwangikamau15@gmail.com>
2023-03-19 13:48:38 -0700

    for loop and if  statement

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - start of program
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+putchar('\n');
+return (0);
+}
