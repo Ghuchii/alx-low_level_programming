9703275f95aeef59f8406bd21030c2374f1b5688
Ghuchii <mwangikamau15@gmail.com>
2023-03-21 09:04:36 -0700

    calling functions prototype

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include<stdio.h>
+#include "main.h"
 /**
- * main - start of function 
- * Return: terminate program
- * 21/03/2023
+ * main - main function
+ *
+ * _putchar: print the alphabet _putchar
+ * return: always 0
  */
 
 int main(void)
 {
-
-	printf("_putchar \n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
