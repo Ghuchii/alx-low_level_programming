6d88d4b4b4897c4c102f84edb28c2a6d36bdbcb1
Ghuchii <mwangikamau15@gmail.com>
2023-04-10 15:59:46 -0700

    creating a static library with various functions

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+		
+/**
+ * _isdigit - this function checks from digits 0-9
+ * @c: parameter to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+0x09 task Static libraries in C
