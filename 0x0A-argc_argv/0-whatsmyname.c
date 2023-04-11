b08497232881aeecc21f46c0866b608de8559df9
Ghuchii <mwangikamau15@gmail.com>
2023-04-10 17:33:58 -0700

    prints its name and new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+* main - start of program  prints its name then followed by a new line.
+* @argc: argument count
+* @argv: arguments
+*
+* 11/04/2023
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+(void)argc;
+
+
+printf("%s\n", argv[0]);
+return (0);
+}
